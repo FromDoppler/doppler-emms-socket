c3be785d6f6e17b5519ae5fc9eb06e77b7a1165c
Andres Moschini <andresmoschini@gmail.com>
2022-10-21 15:54:52 -0300

    fix: fix secret url

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const Config = {
   port: parseInt(process.env.PORT || "3000"),
-  secretUrl: process.env.SECRET_URL || "/SECRET",
+  secretUrl: process.env.SECRET_URL || "SECRET",
   originArrayCors: [
     "http://localhost",
     "http://local-goemms.com",
